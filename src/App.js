5588e2186046daf84d058e3c1a4365c9840af432
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2023-12-27 17:00:56 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Button } from 'semantic-ui-react'
+import axios from "axios";
 
 function App() {
+  // Backend bağlantısı - http isteği
+  // bağlantı sonucu gelen değeri ekranda göstermek
+  // componentler arası veri iletişimi
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+
+
+  const fetchItems = () => {
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => { console.log(response) })
+      .catch((error) => { console.log(error) })
+      .finally(() => { console.log("async işlem bitti") });
+  }
+
   return (
     <div className="App">
       <Button>Click!</Button>
