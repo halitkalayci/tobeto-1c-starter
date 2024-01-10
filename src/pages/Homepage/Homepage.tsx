4d9c7289db0859f9bc841c53c1b014b3393ba63a
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-10 18:20:00 +0300

    localstorage

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 function Homepage() {
 	const cartState = useSelector((state: any) => state.cart);
 	console.log(cartState);
+
+	// useEffect(() => {
+	// 	localStorage.setItem("token", "abc");
+	// 	localStorage.setItem("token", "abc123");
+	// 	let name = localStorage.getItem("name");
+	// 	console.log(name);
+	// 	localStorage.removeItem("token");
+	// 	localStorage.clear();
+
+	// 	let cart = [
+	// 		{id: 1, quantity: 10, price: 50, name: "Ürün 1"},
+	// 		{id: 2, quantity: 5, price: 20, name: "Ürün 2"},
+	// 	];
+	// 	// JSON => string
+	// 	// string => JSON
+	// 	localStorage.setItem("cart", JSON.stringify(cart));
+
+	// 	let cartFromLocalStorage = localStorage.getItem("cart");
+	// 	console.log(cartFromLocalStorage);
+	// 	console.log(JSON.parse(cartFromLocalStorage!));
+	// }, []);
 
 	return (
 		<div>
--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -2,19 +2,22 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
 	name: "cart",
-	initialState: {cartItems: [] as any[]},
+	initialState: {cartItems: JSON.parse(localStorage.getItem("cart")!) || []},
 	reducers: {
 		addToCart: (state, action) => {
 			// aynı üründen 2. kez eklendiğinde listeye eklenmesi yerine adedinin artırılması..
 			state.cartItems.push(action.payload);
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 		removeFromCart: (state, action) => {
 			state.cartItems = state.cartItems.filter(
 				(item: any) => item.id !== action.payload.id,
 			);
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 		clearCart: state => {
 			state.cartItems = [];
+			localStorage.setItem("cart", JSON.stringify(state.cartItems));
 		},
 	},
 });
