a2f2487a9e9ae929ad1e420eb6672d0212565e4b
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-10 17:36:04 +0300

    toolkit reducers

--- src/store/slices/cartSlice.ts
+++ src/store/slices/cartSlice.ts
@@ -2,11 +2,20 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const cartSlice = createSlice({
 	name: "cart",
-	initialState: {cartItems: []},
+	initialState: {cartItems: [] as any[]},
 	reducers: {
-		addToCart: (state, action) => {},
-		removeFromCart: (state, action) => {},
-		clearCart: state => {},
+		addToCart: (state, action) => {
+			// Immer
+			state.cartItems.push(action.payload);
+		},
+		removeFromCart: (state, action) => {
+			state.cartItems = state.cartItems.filter(
+				(item: any) => item.id !== action.payload.id,
+			);
+		},
+		clearCart: state => {
+			state.cartItems = [];
+		},
 	},
 });
 
